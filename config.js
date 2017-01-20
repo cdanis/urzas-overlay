8cf16df652635e26a63de9822704d8fdb1bdaef0
Chris Danis <cdanis@gmail.com>
2017-01-20 10:56:03 -0500

    add config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+  var config = {
+  	apiKey: "AIzaSyDqDSKWpdJJIDBIX5Xq7w24hYss0zliBp8",
+  	authDomain: "urzaslunchbreak.firebaseapp.com",
+  	databaseURL: "https://urzaslunchbreak.firebaseio.com",
+  	storageBucket: "",
+  };
+
+  var firebaseUsername = "urzaslunch@gmail.com";
+  var firebasePassword = "ElkUtyDtI_tr";
