7d04051da8f481d1c31a4f34f7b8847816a84ef4
Gus Prevas <kprevas@gmail.com>
2017-02-02 21:56:21 -0500

    decklist page
    fixes #37

---
+++ decklist.js
@@ -0,0 +1,29 @@
+var config = {
+    apiKey: "AIzaSyDqDSKWpdJJIDBIX5Xq7w24hYss0zliBp8",
+    authDomain: "urzaslunchbreak.firebaseapp.com",
+    databaseURL: "https://urzaslunchbreak.firebaseio.com",
+    storageBucket: "",
+};
+
+firebase.initializeApp(config);
+
+firebase.database().ref('player1').on('value', function (v) {
+    $("#p1").find(".name").text(v.val().name + " - " + v.val().deck);
+});
+firebase.database().ref('player2').on('value', function (v) {
+    $("#p2").find(".name").text(v.val().name + " - " + v.val().deck);
+});
+firebase.database().ref('p1deck').on('value', function (v) {
+    if (v.val()) {
+        var p1 = $("#p1");
+        fillDeck(p1.find(".deck"), v.val(), false);
+        fillDeck(p1.find(".side"), v.val(), true);
+    }
+});
+firebase.database().ref('p2deck').on('value', function (v) {
+    if (v.val()) {
+        var p2 = $("#p2");
+        fillDeck(p2.find(".deck"), v.val(), false);
+        fillDeck(p2.find(".side"), v.val(), true);
+    }
+});
