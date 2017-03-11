2217951f86e753cff3236a1fb4e547196ad2bb2b
Chris Danis <cdanis@gmail.com>
2017-03-11 09:56:27 -0500

    Auto-start OBS from the countdown
    we'll have to open it in its own tab but it should be workable

--- countdown_page.js
+++ countdown_page.js
@@ -8,6 +8,12 @@ timeRef.on('value', function (v) {
     if ($.isNumeric(streamStart) && streamStart > 0) {
         timerId = countdown(streamStart, function (ts) {
             $(".countdown").text(ts.minutes + ":" + ("0" + ts.seconds).slice(-2));
+            // Magic string for auto-starting OBS
+            if (ts.value >= 0) {
+                window.document.title = "ULB GO GO GO GO";
+            } else {
+                window.document.title = "countdown";
+            }
         });
     } else {
         $(".countdown").text("");
